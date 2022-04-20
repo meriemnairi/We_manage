118a92bd43e33a7fdabe921dbf9abe742daa7870
Meriem Nairi <meriemn500@gmail.com>
2022-04-20 15:00:24 +0200

    third commit

--- wemanage/src/components/Posts/Posts.js
+++ wemanage/src/components/Posts/Posts.js
@@ -1,16 +1,21 @@
 import React from "react";
-import Post from'./Post/Post';
+import { useSelector } from "react-redux";
+
+import Post from "./Post/Post";
 import useStyles from "./styles";
 
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+
+  console.log(posts);
+
   return (
-      <>
-        <h1 className={classes.posts}>POSTS</h1>
-        <Post />
-        <Post />
-      </>
- 
+    <>
+      <h1 className={classes.posts}>POSTS</h1>
+      <Post />
+      <Post />
+    </>
   );
 };
 
